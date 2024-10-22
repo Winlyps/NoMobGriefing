82a638515ae17e1e99d7f6b8a083e857e5ef35da
Winlypse <nts3@o2.pl>
2024-10-22 18:35:57 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noMobGriefing/NoMobGriefing.kt
@@ -0,0 +1,14 @@
+package winlyps.noMobGriefing
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoMobGriefing : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
